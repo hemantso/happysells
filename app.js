05dccfea4e04845bcb1d9e4d0913d7e007031185
Hemant soni <sonihemant4741@gmail.com>
2020-05-11 19:29:20 +0530

    good

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(cors());
 
 const port = process.env.PORT || 8000;
 app.use(express.static(path.join(__dirname, './frontend/build')))
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './frontend/build'))
 });
 
