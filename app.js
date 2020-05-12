b4904f5e4f9734151532902520d9e6ef2531ba5b
Hemant soni <sonihemant4741@gmail.com>
2020-05-12 16:18:18 +0530

    make it better

--- app.js
+++ app.js
@@ -48,6 +48,14 @@ app.use("/api", braintreeRoutes)
 app.use("/api", orderRoutes)
 
 const port = process.env.PORT || 8000;
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    });
+  }
 
 app.listen(port, ( ) => {
     console.log(`Server is running on port ${port}`);
