e3e7a11c26a92174b34492ffb2db18a668956739
Hemant soni <sonihemant4741@gmail.com>
2020-05-12 21:52:18 +0530

    make it better

--- app.js
+++ app.js
@@ -43,6 +43,13 @@ app.use("/api", braintreeRoutes)
 app.use("/api", orderRoutes)
 
 const port = process.env.PORT || 8000;
+// Serve any static files
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  });
+
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, 'frontend/build')));
